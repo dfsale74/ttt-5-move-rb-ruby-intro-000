237105549cc602c0e0c0c60a1f7ffd241c892d75
David Sale <dfsale74@outlook.com>
2019-01-19 23:33:15 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,6 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-user_input = "1" - "9"
+user_input = '1'..'9' #Had to Google this - Ranges aren't covered in lesson materials!
 def input_to_index(user_input)
   puts(user.input).to_i
